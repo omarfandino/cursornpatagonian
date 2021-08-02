d9d279168db9ab7ce666b3b3113ccb965a966fe1
omarfandino <fandino.omar@gmail.com>
2021-08-02 18:25:34 -0500

    Integrated Husky

---
+++ src/components/DefaultButton/DefaultButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, TouchableOpacity, ViewStyle } from 'react-native';
+import { colors } from '../../utils/theme';
+import styles from './style';
+
+interface Props {
+  additionalStyle?: ViewStyle;
+  color?: string;
+  onPress: () => void;
+  text: string;
+  textSize?: number;
+}
+
+const DefaultButton = ({ additionalStyle, color, onPress, text, textSize }: Props) => {
+  return (
+    <TouchableOpacity
+      style={[styles.mainContainer, additionalStyle, { backgroundColor: color }]}
+      onPress={onPress}
+    >
+      <Text style={{ fontSize: textSize }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+DefaultButton.defaultProps = {
+  additionalStyle: {},
+  color: colors.mainOrange,
+  textSize: 14,
+};
+
+export default DefaultButton;
