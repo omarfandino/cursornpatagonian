189f96c715954cb245c0a957358fd39be1ab61b4
omarfandino <fandino.omar@gmail.com>
2021-09-01 17:14:01 -0500

    Implemented capability to keep a fake session open so the user is redirect directly to the Main Tabs Added a visual indicator to tell the users when they don't have internet, using the 'react-native-netinfo' library. Implemented the FlatList component on Home screen to display a basic list of the books fetched from the API

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,51 +1,88 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, SafeAreaView, ScrollView, View } from 'react-native';
+import { ActivityIndicator, Alert, FlatList, View } from 'react-native';
+import { useNetInfo } from '@react-native-community/netinfo';
 
-import { DefaultButton, Separator, Typography } from '../../components';
+import { DefaultButton, Header, Separator, Typography } from '../../components';
 import styles from './styles';
 
 import { getAllBooks } from '../../services';
 import { goToScreen } from '../../navigation/controls';
+import { colors } from '../../utils/theme';
 
 const goToExperimentalScreen = () => {
   goToScreen('Experimental');
 };
 
+const ListItem = ({ title }: { title: string }) => (
+  <View style={styles.listItemContainer}>
+    <Typography>{title}</Typography>
+  </View>
+);
+
+// @ts-ignore
+const renderFlatlistItem = ({ item }) => <ListItem title={item.title} />;
+
 const HomeScreen = () => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const dis = async () => {
-      try {
-        const { success, data } = await getAllBooks();
-        if (success) {
-          setBooks(data);
-        } else {
-          Alert.alert('Error getting books on Home Screen');
-        }
-      } catch (error) {
-        console.log('Error getting books on Home Screen', error);
+  const netInfo = useNetInfo();
+
+  const getBooksData = async () => {
+    setLoading(true);
+    try {
+      const { success, data } = await getAllBooks();
+      if (success) {
+        setBooks(data);
+      } else {
         Alert.alert('Error getting books on Home Screen');
       }
-    };
-    dis();
+    } catch (error) {
+      console.log('Error getting books on Home Screen', error);
+      Alert.alert('Error getting books on Home Screen');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBooksData();
   }, []);
 
-  console.log('Inside HomeScreen');
+  if (!netInfo.isConnected) {
+    return (
+      <View style={styles.wholeScreenCenter}>
+        <Typography size={20}>You don't have internet :'(</Typography>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <Header showBackButton={false} title="Home Screen" />
+        <View style={styles.wholeScreenCenter}>
+          <ActivityIndicator size="large" color={colors.mainOrange} />
+        </View>
+      </>
+    );
+  }
 
   return (
     <>
-      <SafeAreaView />
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.mainContainer}>
-          <Typography size={20} variant="bold">
-            Home Screen
-          </Typography>
-          <Separator size={10} />
-          <DefaultButton text="Go To Experimental Screen" onPress={goToExperimentalScreen} />
-          <Typography>{JSON.stringify(books, null, 2)}</Typography>
-        </View>
-      </ScrollView>
+      <Header showBackButton={false} title="Home Screen" />
+      <View style={styles.mainContainer}>
+        <Separator size={20} />
+        <DefaultButton text="Go To Experimental Screen" onPress={goToExperimentalScreen} />
+        <Separator size={20} />
+        <FlatList
+          refreshing={loading}
+          onRefresh={getBooksData}
+          data={books}
+          renderItem={renderFlatlistItem}
+          style={styles.flatList}
+        />
+      </View>
     </>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  flatList: {
+    flex: 1,
+    width: '100%',
+  },
   mainContainer: {
     alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
     width: '100%',
   },
-  scrollView: {
+  listItemContainer: {
+    height: 30,
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    width: '96%',
+  },
+  wholeScreenCenter: {
+    alignItems: 'center',
     flex: 1,
+    justifyContent: 'center',
     width: '100%',
   },
 });
